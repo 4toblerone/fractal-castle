4b146e612e3157b51a6bcc604f555923eb257899
4toblerone <sasa.s.trifunovic@gmail.com>
2013-10-16 18:22:40 +0200

    acl

--- app/views.py
+++ app/views.py
@@ -84,7 +84,7 @@ def admin():
         key.key = keyname
         key.set_metadata("Content-Type", 'image/jpeg')
         key.set_metadata("Cache-Control" , 'max-age=910000')
-        key.set_acl("public-read")
+        key.set_acl('public-read')
         key.set_contents_from_file(uploadedphoto)
         #creting new Photo instance and adding it to parent PhotoProject
         newphoto = Photo(photokey=photoname.replace(" ", "").lower(), 
