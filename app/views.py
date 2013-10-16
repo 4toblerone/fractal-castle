99608fec6d35875c58269f4dee8ddd26dffc7d05
4toblerone <sasa.s.trifunovic@gmail.com>
2013-10-16 18:31:43 +0200

    policy added

--- app/views.py
+++ app/views.py
@@ -84,8 +84,8 @@ def admin():
         key.key = keyname
         key.set_metadata("Content-Type", 'image/jpeg')
         key.set_metadata("Cache-Control" , 'max-age=910000')
-        key.set_acl('public-read')
-        key.set_contents_from_file(uploadedphoto)
+        #key.set_acl('public-read')
+        key.set_contents_from_file(uploadedphoto, policy='public-read')
         #creting new Photo instance and adding it to parent PhotoProject
         newphoto = Photo(photokey=photoname.replace(" ", "").lower(), 
                             name=photoname, placenumber=placenumber, 
