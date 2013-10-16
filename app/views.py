7dc3356c1065a0e596ad84691e6755e0ba4d09f8
4toblerone <sasa.s.trifunovic@gmail.com>
2013-10-16 14:46:17 +0200

    try except added for OperationalError

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from models import Photo, User, PhotoProject
 from fractalcastle.app import db, lm
 from fractalcastle.app import app2 as app
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask.ext.sqlalchemy.exc import OperationalError
 from boto.s3.key import Key
 from s3connect import getbucket as bucket
 import inspect
@@ -23,7 +24,13 @@ def index():
 @app.route('/<path:projectkey>')
 def project(projectkey):
 
-    photos = returnPPPhotosUrls(projectkey)
+    attempts =0
+    for attempts in range(3):
+        try:
+            photos = returnPPPhotosUrls(projectkey)
+        except _mysql_exceptions.OperationalError:
+            attempts+=1
+        break
     return render_template("project.html", photosUrl=photos, length = len(photos),
                             projectList=sortPhotosProjects(returnPublishedProjects()))
 
